e96a3d5bf1dd5aa4ce8b62a32a8139bfe0181262
discovery-design <57754007+discovery-design@users.noreply.github.com>
2022-11-03 16:19:08 +0300

    Add files via upload

---
+++ Leetcode-SecondMostRecentActivity-Medium.sql
@@ -0,0 +1,63 @@
+-- Question 109
+-- Table: UserActivity
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | username      | varchar |
+-- | activity      | varchar |
+-- | startDate     | Date    |
+-- | endDate       | Date    |
+-- +---------------+---------+
+-- This table does not contain primary key.
+-- This table contain information about the activity performed of each user in a period of time.
+-- A person with username performed a activity from startDate to endDate.
+
+-- Write an SQL query to show the second most recent activity of each user.
+
+-- If the user only has one activity, return that one. 
+
+-- A user can't perform more than one activity at the same time. Return the result table in any order.
+
+-- The query result format is in the following example:
+
+-- UserActivity table:
+-- +------------+--------------+-------------+-------------+
+-- | username   | activity     | startDate   | endDate     |
+-- +------------+--------------+-------------+-------------+
+-- | Alice      | Travel       | 2020-02-12  | 2020-02-20  |
+-- | Alice      | Dancing      | 2020-02-21  | 2020-02-23  |
+-- | Alice      | Travel       | 2020-02-24  | 2020-02-28  |
+-- | Bob        | Travel       | 2020-02-11  | 2020-02-18  |
+-- +------------+--------------+-------------+-------------+
+
+-- Result table:
+-- +------------+--------------+-------------+-------------+
+-- | username   | activity     | startDate   | endDate     |
+-- +------------+--------------+-------------+-------------+
+-- | Alice      | Dancing      | 2020-02-21  | 2020-02-23  |
+-- | Bob        | Travel       | 2020-02-11  | 2020-02-18  |
+-- +------------+--------------+-------------+-------------+
+
+-- The most recent activity of Alice is Travel from 2020-02-24 to 2020-02-28, before that she was dancing from 2020-02-21 to 2020-02-23.
+-- Bob only has one record, we just take that one.
+
+--Solution:
+
+Select * From (Select username,activity,startDate,endDate,rank()Over(partition by username order by startdate desc) as RNK,
+Count(username)Over(partition by username ) as CNT From UserActivity
+) as A 
+Where A.RNK=2 Or (CNT)=1
+
+
+--Results
+
+username                                           activity                                           startDate  endDate    RNK                  CNT
+-------------------------------------------------- -------------------------------------------------- ---------- ---------- -------------------- -----------
+Alice                                              Dancing                                            2020-02-21 2020-02-23 2                    3
+Bob                                                Travel                                             2020-02-11 2020-02-18 1                    1
+
+(2 rows affected)
+
+
+Completion time: 2022-11-03T16:19:05.1321685+03:00
